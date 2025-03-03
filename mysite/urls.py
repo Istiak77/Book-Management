cef16c30e7fc27a7970d1d7e2eae4d154b2df452
Istiak77 <ahmedpranto666@gmail.com>
2025-03-03 20:52:13 +0600

    fixed urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,7 +14,7 @@ def custom_logout(request):
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('book/', include('food.urls')),
+    path('', include('food.urls')),
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', custom_logout, name='logout'), 
